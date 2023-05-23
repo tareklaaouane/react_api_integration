2f4c95747260f1a61170734a61711354570d08dc
tareklaaouane <tlaaouane@gmail.com>
2023-05-23 02:16:28 +0100

    api integrated

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const url = "https://jsonplaceholder.typicode.com/posts"
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        loading ? <Text>loading .......</Text> : (
+          data.map((post) => (
+            <view style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+              <Text style={{ fontSize: 30, fontWeight: "bold" }}>{post.title}</Text>
+              <Text style={{ fontSize: 15, fontWeight: "blue" }}>{post.body}</Text>
+            </view>
+          ))
+        )
+      }
     </View>
   );
 }
